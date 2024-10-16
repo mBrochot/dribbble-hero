3f138d7daf488975f63b15a82e3b475b4baf9063
mBrochot <brochot.mathieu@gmail.com>
2024-10-16 22:39:44 +0200

    update handleLetterG to transform multiple g's into words

--- app/utils/index.tsx
+++ app/utils/index.tsx
@@ -1,19 +1,27 @@
-import type { ReactNode } from 'react';
+import { ReactNode, Fragment } from 'react';
+import clsx from 'clsx';
 
-export const handleLetterG = (word: string): ReactNode => {
-  if (word.includes('g')) {
-    const index = word.indexOf('g');
-    const firstPart = word.substring(0, index);
-    const secondPart = word.substring(index + 1);
+export const handleLetterG = (words: string, textStyle?: string): ReactNode => {
+  if (words.match(/g|ğ/)) {
+    const parts = words.split(/(g|ğ)/);
+
     return (
       <>
-        {firstPart}
-        <span className="font-serif text-base">{word[index]}</span>
-        {secondPart}
+        {parts.map((part, index) => (
+          <Fragment key={index}>
+            {part === 'g' || part === 'ğ' ? (
+              <span className={clsx('font-serif', textStyle ?? 'text-[14px]')}>
+                {part}
+              </span>
+            ) : (
+              part
+            )}
+          </Fragment>
+        ))}
       </>
     );
   }
-  return word;
+  return words;
 };
 
 export const getRandomBetween = (min: number, max: number) => {
