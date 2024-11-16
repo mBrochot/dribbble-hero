ccb6d4cf07e47c1ec5357f4ea839309a104d6168
mBrochot <brochot.mathieu@gmail.com>
2024-11-16 01:16:11 +0100

    fix footer position on mobile

--- app/components/FooterSection/Footer.tsx
+++ app/components/FooterSection/Footer.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react';
 
 const Footer = (): ReactNode => {
   return (
-    <div className="px-8 pb-12 flex justify-center md:justify-start">
+    <div className="pt-40 px-8 pb-12 flex justify-center md:justify-start">
       <ul className="text-sm text-custom-grey">
         <li>© 2024 Mathieu Brochot</li>
       </ul>
